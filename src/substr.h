67b9997d9d100f5e6de851bb25bf89aefd72b44a
Geympadek <andreltsov08@outlook.com>
2024-06-25 10:06:58 +0400

    added hex functions to substr class

--- src/substr.h
+++ src/substr.h
@@ -3,6 +3,7 @@
 #include <type_traits>
 #include <string>
 #include <cassert>
+#include <stdexcept>
 
 namespace engix
 {
@@ -73,6 +74,120 @@ namespace engix
         String toString() const noexcept
         {
             return {_begin, _end};
+        }
+
+        static constexpr uint8_t hexFromChar(C c)
+        {
+            if constexpr (std::is_same_v<C, char>)
+            {
+                switch (tolower(c))
+                {
+                    case '0': return 0;
+                    case '1': return 1;
+                    case '2': return 2;
+                    case '3': return 3;
+                    case '4': return 4;
+                    case '5': return 5;
+                    case '6': return 6;
+                    case '7': return 7;
+                    case '8': return 8;
+                    case '9': return 9;
+                    case 'a': return 0xa;
+                    case 'b': return 0xb;
+                    case 'c': return 0xc;
+                    case 'd': return 0xd;
+                    case 'e': return 0xe;
+                    case 'f': return 0xf;
+                }
+            }
+            else if constexpr (std::is_same_v<C, char16_t>) 
+            {
+                switch (tolower(c))
+                {
+                    case u'0': return 0;
+                    case u'1': return 1;
+                    case u'2': return 2;
+                    case u'3': return 3;
+                    case u'4': return 4;
+                    case u'5': return 5;
+                    case u'6': return 6;
+                    case u'7': return 7;
+                    case u'8': return 8;
+                    case u'9': return 9;
+                    case u'a': return 0xa;
+                    case u'b': return 0xb;
+                    case u'c': return 0xc;
+                    case u'd': return 0xd;
+                    case u'e': return 0xe;
+                    case u'f': return 0xf;
+                }
+            }
+            else if constexpr (std::is_same_v<C, char32_t>)
+            {
+                switch (tolower(c))
+                {
+                    case U'0': return 0;
+                    case U'1': return 1;
+                    case U'2': return 2;
+                    case U'3': return 3;
+                    case U'4': return 4;
+                    case U'5': return 5;
+                    case U'6': return 6;
+                    case U'7': return 7;
+                    case U'8': return 8;
+                    case U'9': return 9;
+                    case U'a': return 0xa;
+                    case U'b': return 0xb;
+                    case U'c': return 0xc;
+                    case U'd': return 0xd;
+                    case U'e': return 0xe;
+                    case U'f': return 0xf;
+                }
+            }
+            else if constexpr (std::is_same_v<C, wchar_t>)
+            {
+                switch (tolower(c))
+                {
+                    case L'0': return 0;
+                    case L'1': return 1;
+                    case L'2': return 2;
+                    case L'3': return 3;
+                    case L'4': return 4;
+                    case L'5': return 5;
+                    case L'6': return 6;
+                    case L'7': return 7;
+                    case L'8': return 8;
+                    case L'9': return 9;
+                    case L'a': return 0xa;
+                    case L'b': return 0xb;
+                    case L'c': return 0xc;
+                    case L'd': return 0xd;
+                    case L'e': return 0xe;
+                    case L'f': return 0xf;
+                }
+            }
+            throw std::invalid_argument("Unexpected char in hex conversion.");
+        }
+
+        static constexpr uint32_t hexFromString(Substr str)
+        {
+            uint32_t hex = 0;
+            
+            for (auto c : str)
+            {
+                auto value = hexFromChar(c);
+                if (hex == 0 && value == 0)
+                {
+                    continue;
+                }
+                hex <<= 4;
+                hex += value;
+            }
+            return hex;
+        }
+        constexpr uint32_t toHex() const
+        {
+            return hexFromString(*this);
         }
 
         static constexpr C toupper(C c) noexcept
