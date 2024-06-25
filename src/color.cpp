517fd93b80209a0aa8043447173f9e6a799c1ea1
Geympadek <andreltsov08@outlook.com>
2024-06-25 10:07:40 +0400

    replaced static functions with functions from substr

--- src/color.cpp
+++ src/color.cpp
@@ -13,47 +13,6 @@ const Color Color::BLUE(0, 0, 255);
 const Color Color::CYAN(0, 255, 255);
 const Color Color::TRANSPARENT(0, 0, 0, 0);
 
-static constexpr uint8_t hexFromChar(char c)
-{
-    switch (Substr<char>::tolower(c))
-    {
-    case '0': return 0;
-    case '1': return 1;
-    case '2': return 2;
-    case '3': return 3;
-    case '4': return 4;
-    case '5': return 5;
-    case '6': return 6;
-    case '7': return 7;
-    case '8': return 8;
-    case '9': return 9;
-    case 'a': return 0xa;
-    case 'b': return 0xb;
-    case 'c': return 0xc;
-    case 'd': return 0xd;
-    case 'e': return 0xe;
-    case 'f': return 0xf;
-    }
-    throw std::invalid_argument("Unexpected char in hex conversion.");
-}
-
-static constexpr uint32_t hexFromString(Substr<char> str)
-{
-    uint32_t hex = 0;
-    
-    for (auto c : str)
-    {
-        if (hex == 0 && c == '0')
-        {
-            continue;
-        }
-        hex <<= 4;
-        auto value = hexFromChar(c);
-        hex += value;
-    }
-    return hex;
-}
-
 void engix::Color::loadFromString(Substr<char> str) noexcept
 {
     auto size = str.findSize();
@@ -62,7 +21,7 @@ void engix::Color::loadFromString(Substr<char> str) noexcept
     if (str[0] == '#')
         str.begin()++;
     
-    uint32_t color = hexFromString(str);
+    uint32_t color = str.toHex();
     switch (size)
     {
     case 6:
