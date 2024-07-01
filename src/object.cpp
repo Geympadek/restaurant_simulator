22e7f402fb36e7484233d62f969b8007dfa43d86
Geympadek <andreltsov08@outlook.com>
2024-07-01 20:44:06 +0400

    added full colliders and resolve collision function

--- src/object.cpp
+++ src/object.cpp
@@ -1,1 +1,57 @@
 #include "object.h"
+
+engix::Object::Object(std::shared_ptr<Texture> texture)
+: Drawable(std::move(texture))
+{
+    _width /= 4;
+}
+
+engix::Object::Object(std::shared_ptr<Texture> texture, int width, int height)
+: Drawable(std::move(texture), width, height)
+{
+}
+
+void engix::Object::render(const Camera &cam) const
+{
+    Drawable::renderClip(cam, Rect(static_cast<int>(_direction) * _width, 0, _width, _height));
+#ifdef DISPLAY_COLLIDERS
+    _collider.render(_position, _scale, Color::BLUE, cam);
+#endif
+}
+
+void engix::Object::collide(Object &a, Object &b)
+{
+    auto arect = a._collider.toRect(a._position, a._scale);
+    auto brect = b._collider.toRect(b._position, b._scale);
+
+    auto intersection = Rect::intersection(arect, brect);
+    if (!intersection)
+    {
+        return;
+    }
+    std::cout << "intersection!\n";
+
+    int moveableCount = 0;
+    if (a._moveable) moveableCount++; 
+    if (b._moveable) moveableCount++;
+
+    if (intersection.width < intersection.height)
+    {
+        auto movement = std::max(intersection.width, 1);
+        auto sign = arect.start.x < brect.start.x ? 1 : -1;
+        
+        if (a._moveable)
+            a._position.x -= movement * sign;
+        if (b._moveable)
+            b._position.x += movement * sign;
+    } else
+    {
+        auto movement = std::max(intersection.height, 1);
+        auto sign = arect.start.y < brect.start.y ? 1 : -1;
+        
+        if (a._moveable)
+            a._position.y -= movement * sign;
+        if (b._moveable)
+            b._position.y += movement * sign;
+    }
+}
--- src/object.h
+++ src/object.h
@@ -1,20 +1,38 @@
 #pragma once
 
 #include "drawable.h"
+#include "collider.h"
+
+#define DISPLAY_COLLIDERS
 
 namespace engix
 {
-    using Collider = Rect;
+    enum class Direction {DOWN = 0, UP = 1, LEFT = 2, RIGHT = 3 };
+    
     class Object : public Drawable
     {
     public:
         Object() {}
-        Object(std::shared_ptr<Texture> texture) : Drawable(std::move(texture)) {}
-        Object(std::shared_ptr<Texture> texture, int width, int height) : Drawable(std::move(texture), width, height) {}
+        Object(std::shared_ptr<Texture> texture);
+        Object(std::shared_ptr<Texture> texture, int width, int height);
+
+        virtual void render(const Camera& cam) const override;
+
+        static bool checkCollision(const Object& a, const Object& b) {return Collider::checkCollision(a._position, a._scale, a._collider, b._position, b._scale, b._collider);}
+        static void collide(Object& a, Object& b);
     public:
         Collider collider() const {return _collider;}
         void collider(Collider collider) {_collider = collider;}
+
+        bool moveable() const {return _moveable;}
+        void moveable(bool moveable) {_moveable = moveable;}
+
+        Direction direction() const {return _direction;}
+        void direction(Direction dir) {_direction = dir;}
     protected:
         Collider _collider;
+        bool _moveable = true;
+
+        Direction _direction = Direction::DOWN;
     };
 }
