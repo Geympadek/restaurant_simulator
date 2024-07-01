521832684ee5abe68a48f2191061dd8f120d2fb4
Geympadek <andreltsov08@outlook.com>
2024-07-01 20:45:37 +0400

    added animations path

--- src/assets.h
+++ src/assets.h
@@ -8,6 +8,7 @@ namespace assets
     const Path FONTS_PATH = RESOURCES_PATH + "fonts/";
     const Path AUDIO_PATH = RESOURCES_PATH + "audio/";
     const Path TEXTURES_PATH = RESOURCES_PATH + "textures/";
+    const Path ANIMATIONS_PATH = RESOURCES_PATH + "animations/";
     const Path BOX_TEMPLATE_PATH = RESOURCES_PATH + "templates/";
     const Path DATA_PATH = RESOURCES_PATH + "data/";
 }
