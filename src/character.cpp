67b1269cab842b759407108f94856863b23c25af
Geympadek <andreltsov08@outlook.com>
2024-07-04 21:37:16 +0400

    Implemented Character class

--- src/character.cpp
+++ src/character.cpp
@@ -1,16 +1,51 @@
 #include "character.h"
 
-engix::Character::Character(shared_ptr<Texture> texture) 
-: Object(std::move(texture))
+engix::Character::Character(Pack &pack)
+: Object(pack.textures["stand"]), _pack(pack)
 {
+    _activeAnimation = pack.animations["walk"];
+    _animationTimer.start();
 }
 
-engix::Character::Character(shared_ptr<Texture> texture, int width, int height)
-: Object(std::move(texture), width, height)
+void engix::Character::render(const Camera &camera) const
 {
+    if (_activeAnimation == nullptr)
+    {
+        Object::render(camera);
+    }
+    else
+    {
+        renderAnimClip(camera, Rect(static_cast<int>(_direction) * _width, 0, _width, _height));
+    }
 }
 
 void engix::Character::update(Input &input)
 {
     Object::update(input);
+
+    if (_activeAnimation != nullptr)
+    {
+        if (_animationTimer.millis() > _activeAnimation->duration())
+        {
+            _animationTimer.restart();
+        }
+    }
+}
+
+void engix::Character::renderAnim(const Camera &cam) const
+{
+    Vector2d pos;
+    double scale;
+    renderInfo(cam, pos, scale);
+    
+    _activeAnimation->render(_animationTimer.millis(), pos, scale, _rotation, _center, _flip);
+}
+
+void engix::Character::renderAnimClip(const Camera &cam, Rect clip) const
+{
+    Vector2d pos;
+    double scale;
+    renderInfo(cam, pos, scale);
+
+    _activeAnimation->render(_animationTimer.millis(), pos, clip, scale, _rotation, _center, _flip);
 }
--- src/character.h
+++ src/character.h
@@ -1,17 +1,23 @@
 #pragma once
 
 #include "object.h"
+#include "animation.h"
+#include "pack.h"
 
 namespace engix
 {
     class Character : public Object
     {
     public:
-        Character() {}
-        Character(shared_ptr<Texture> texture);
-        Character(shared_ptr<Texture> texture, int width, int height);
-
+        Character(Pack& pack);
+        virtual void render(const Camera& camera) const override;
         virtual void update(Input& input) override;
     protected:
+        void renderAnim(const Camera& cam) const;
+        void renderAnimClip(const Camera& cam, Rect clip) const;
+    protected:
+        Pack& _pack;
+        std::shared_ptr<Animation> _activeAnimation;
+        Clock _animationTimer;
     };
 }
